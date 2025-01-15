f8fc5b0f8642a691105f31221429374e9ac97093
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-01-15 17:37:52 +0530

    hashing PSAP

--- DSA/Hashing/LargestSubArrayWith0sum.java
+++ DSA/Hashing/LargestSubArrayWith0sum.java
@@ -1,6 +1,22 @@
 package DSA.Hashing;
 
+import java.util.HashMap;
+
 public class LargestSubArrayWith0sum {
     
     
+    public static void main(String[] args) {
+        int arr[] = {15,-2,2,-8,1,7,10,23};
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int sum=0,l = 0 ;
+        for (int i = 0; i < arr.length; i++) {
+            sum +=arr[i];
+            if(map.containsKey(sum)){
+                l = Math.max(l , i-map.get(sum));
+            }else{
+                map.put(sum, i);
+            }
+        }
+        System.out.println("largest subarr length with sum 0 : "+l);
+    }
 }
--- DSA/Hashing/MajorityElements.java
+++ DSA/Hashing/MajorityElements.java
@@ -1,6 +1,24 @@
 package DSA.Hashing;
 
+import java.util.*;
+
 public class MajorityElements {
-    
+    public static void main(String[] args) {
+        int arr[] = {1,3,2,5,1,3,1,5,1};
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(hm.containsKey(arr[i])){
+                hm.put(arr[i], hm.get(arr[i])+1);
+            }else{
+                hm.put(arr[i], 1);
+            }
+        }
+       Set<Integer> keySet = hm.keySet();
+        for (int key : keySet) {
+            if(hm.get(key)>arr.length/3){
+                System.out.println(key);
+            }
+        }
+    }
     
 }
