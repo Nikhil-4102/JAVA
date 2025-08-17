6b54de440d2340d3e73538b16e81f0d823176437
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-08-18 00:02:12 +0530

    BST codes added

--- DSA/BST/DeleteBST.java
+++ DSA/BST/DeleteBST.java
@@ -30,38 +30,42 @@ public class DeleteBST {
     }
 
     public static Node delete(Node root , int val){
-        if(root.data>val){
-            root.left = delete(root.left, val);
-        }else if(root.data<val){
-            root.right = delete(root.right, val);
-        }else{
-           // case-1 --- leaf node 
-           if(root.left==null && root.right==null){
-            return null;
-           }
-           // case-2 --- having one child
-           if(root.left==null){
-            return root.right;
-           }
-           else if(root.right==null){
-            return root.left;
-           }
-           // case-3 --- having 2 children
-           Node IS = findInorderSuccessor(root.right);
-           root.data= IS.data;
-           root.right = delete(root.right, IS.data);
+        if(root.data > val){
+            root.left = delete(root.left , val);
         }
-        return root;
+        else if(root.data < val){
+            root.right = delete(root.right , val);
+        }else{
+            // case where we found val = root.data
+            // condition -1 ---> No chldren
+            if(root.left == null && root.right == null){
+                return null ;
+            }
+            // condition -2 ---> 1 child
+            if(root.left == null){
+                return root.right;
+            }
+            else if(root.right == null){
+                return root.left;
+            }
+            // condition -3 --->
+            Node IS = findInorderSuccesser(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right , IS.data);
+        }
+        return root ;
     }
 
-    public static Node findInorderSuccessor(Node root){
-        while (root.left != null) {
+    public static Node findInorderSuccesser(Node root){
+        while(root.left != null){
             root = root.left;
         }
         return root;
     }
+
+   
     public static void main(String[] args) {
-        int values[]= {5,1,3,4,2,7};
+        int values[]= {8,5,3,1,4,6,10,11,14};
         Node root = null;
         for(int i = 0 ; i <values.length;i++){
             root = insert(root , values[i]);
@@ -70,7 +74,7 @@ public class DeleteBST {
         inorder(root);
         System.out.println();
  
-        delete(root, 5);
+        delete(root, 6);
         System.out.println("after deleting ...");
         inorder(root);
     } 
