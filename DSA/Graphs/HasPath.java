0362694e5d4bb90c2fa3329ef5be0535036a22b1
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-08-23 22:22:41 +0530

    graph hasPath done

--- DSA/Graphs/HasPath.java
+++ DSA/Graphs/HasPath.java
@@ -1,5 +1,64 @@
 package DSA.Graphs;
 
+import java.util.ArrayList;
+
 public class HasPath {
+
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    public static boolean hasPath(ArrayList<Edge>[] graph , int src , int dest , boolean visited[]){
+
+        if(src == dest) return true;
+
+        visited[src] = true ;
+
+        for(int i = 0 ; i<graph[src].size() ; i++){
+            Edge e = graph[src].get(i);
+            if(!visited[e.dest] && hasPath(graph, e.dest, dest, visited)){
+                return true;
+            }
+        }
+
+
+       return false;
+    }
+
+    public static void main(String[] args) {
+
+        int V = 5;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        for (int i = 0; i < V; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 5));
+
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 4, 2));
+
+        graph[3].add(new Edge(3, 2, 1));
+        graph[3].add(new Edge(3, 1, 3));
+
+        graph[4].add(new Edge(4, 2, 2));
+
+        System.out.println(hasPath(graph, 0, 4, new boolean[V]));
+
+    }
+    
     
 }
