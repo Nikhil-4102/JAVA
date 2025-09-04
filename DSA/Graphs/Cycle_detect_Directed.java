d2b405430dfdc047eb9afefc40b684c49e36731e
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-09-05 00:43:13 +0530

    graph - bellmanFord ,  prims , cheapest fight  added

--- DSA/Graphs/Cycle_detect_Directed.java
+++ DSA/Graphs/Cycle_detect_Directed.java
@@ -1,5 +1,60 @@
 package DSA.Graphs;
 
+import java.util.ArrayList;
+
 public class Cycle_detect_Directed {
     
+    public static class Edge{
+        int src ;
+        int dest ; 
+        int wt ;
+         public Edge(int s , int d , int w){
+            this.src  = s ;
+            this.dest = d ;
+            this.wt = w ;
+         }
+    }
+    public static boolean is_Cycle(ArrayList<Edge>[]graph){
+        boolean visited [] = new boolean[graph.length];
+        boolean stack [] = new boolean[graph.length];
+        for(int i = 0 ; i < graph.length ; i++){
+            if(!visited[i]){
+                if(is_Cycle_Util(graph , i , visited , stack)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean is_Cycle_Util(ArrayList<Edge>graph [] , int curr , boolean visited [] , boolean stack []){
+        visited[curr] = true ;
+        stack[curr] = true;
+        for(int i = 0 ; i < graph[curr].size() ; i++){
+            Edge e = graph[curr].get(i);
+
+            if(stack[e.dest]){
+                return true;
+            }
+            if(!visited[e.dest] && is_Cycle_Util(graph, e.dest, visited, stack)){
+                return true ;
+            }
+        }
+        stack[curr] = false ;
+        return false;
+    }
+    public static void main(String[] args) {
+        int V = 4 ;
+        ArrayList<Edge>graph[] = new ArrayList[V];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] =new ArrayList<>();
+        }
+        graph[1].add(new Edge(1,0,1));
+        graph[0].add(new Edge(0,2,1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[3].add(new Edge(3, 0, 1));
+
+        System.out.println(is_Cycle(graph));
+    }
+
 }
--- DSA/Graphs/Topological_Srot.java
+++ DSA/Graphs/Topological_Srot.java
@@ -1,19 +1,70 @@
 package DSA.Graphs;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Topological_Srot {
     
     public static class  Edge{
+        int src;
+        int dest;
+        int wt;
 
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
     }
 
-    public static void topSort(){
-
+    public static void topSort(ArrayList<Edge>graph []){
+          
+        boolean visited[] = new boolean[graph.length];
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < graph.length; i++) {
+            if(!visited[i]){
+                topSortUtil(graph , i , visited , s);
+            }
+        }
+        while (!s.isEmpty()) {
+            System.out.print(s.pop()+" ");
+        }
     }
 
-    public static void topSortUtil(){
-        
+    public static void topSortUtil(ArrayList<Edge>graph[] , int curr , boolean visited [] , Stack s){
+        visited[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if(!visited[e.dest]){
+                topSortUtil(graph, e.dest, visited, s);
+            }
+        }
+        s.push(curr);
     }
     public static void main(String[] args) {
         
+        int V = 5;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        for (int i = 0; i < V; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 5));
+
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 4, 2));
+
+        graph[3].add(new Edge(3, 2, 1));
+        graph[3].add(new Edge(3, 1, 3));
+
+        graph[4].add(new Edge(4, 2, 2));
+        
+        topSort(graph);
+
     }
 }
