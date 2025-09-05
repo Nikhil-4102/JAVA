3b847d43f442eb08afbbb5cc14b15b18361db3a5
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-09-05 13:36:25 +0530

    graph - dijkstra code completed successfully

--- DSA/Graphs/All_Paths.java
+++ DSA/Graphs/All_Paths.java
@@ -1,5 +1,55 @@
 package DSA.Graphs;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class All_Paths {
     
+     public static class  Edge{
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    public static void printAllPaths(ArrayList<Edge> graph[] , int src , int dest , String path){
+        if(src == dest){
+            System.out.println(path+dest);
+            return ;
+        }
+        for(int i = 0 ; i < graph[src].size();i++){
+            Edge e = graph[src].get(i);
+            printAllPaths(graph, e.dest, dest, path+src);
+        }
+    }
+    public static void main(String[] args) {
+        
+        int V = 6;
+        ArrayList<Edge>[] graph = new ArrayList[6];
+        for (int i = 0; i < V; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 3, 1));
+
+        graph[2].add(new Edge(2, 3, 1));
+        
+        graph[3].add(new Edge(3, 1, 1));
+
+        graph[4].add(new Edge(4, 1, 1));
+        graph[4].add(new Edge(4, 0, 1));
+
+        graph[5].add(new Edge(5, 2, 1));
+        graph[5].add(new Edge(5, 0, 1));
+
+        int src = 5 , dest = 1;
+        
+        printAllPaths(graph , src , dest , " ");
+
+    }
 }
--- DSA/Graphs/Dijkstra.java
+++ DSA/Graphs/Dijkstra.java
@@ -1,16 +1,93 @@
 package DSA.Graphs;
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 public class Dijkstra {
 
     public static class Edge{
-        
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+    public static class Pair implements Comparable<Pair> {
+         int n ; 
+         int path ;
+           public Pair(int n , int path){
+            this.n = n ; 
+            this.path = path ;
+           }
+        @Override
+        public int compareTo(Pair p2){
+            return this.path - p2.path ;
+        }   
         
     }
-    public static void dijkstra(){
-        
+    public static void dijkstra(ArrayList<Edge>graph [] , int src ){
+        int dist [] = new int[graph.length];
+        for (int i = 0; i < dist.length; i++) {
+            if(i != src){
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+    boolean visited[] = new boolean[graph.length];
+    PriorityQueue<Pair> pq = new PriorityQueue<>();
+     pq.add(new Pair(src, 0));
+
+      while(!pq.isEmpty()) {
+          Pair curr = pq.remove();
+          if(!visited[curr.n]){
+            visited[curr.n] = true;
+            for(int i = 0 ; i< graph[curr.n].size() ; i++){
+                Edge e = graph[curr.n].get(i);
+
+                int u = e.src ;
+                int v = e.dest ;
+                int w = e.wt ;
+
+                if(dist[u] + w < dist[v]){
+                    dist[v] = dist[u] + w ;
+                    pq.add(new Pair(v, dist[v]));
+                }
+            }
+          }  
+    }
+
+    for (int i = 0; i < dist.length; i++) {
+        System.out.print(dist[i]+" ");
+    }
 
     }
     public static void main(String[] args) {
+         int V = 6;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        for (int i = 0; i < V; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+
+        graph[1].add(new Edge(1, 3, 7));
+        graph[1].add(new Edge(1, 2, 1));
+       
+
+        graph[2].add(new Edge(2, 4, 3));
         
+
+        graph[3].add(new Edge(3, 5, 1));
+
+        graph[4].add(new Edge(4, 3, 2));
+        graph[4].add(new Edge(4, 5, 5));
+        
+        int src = 0;
+        dijkstra(graph, src);
     }
 }
